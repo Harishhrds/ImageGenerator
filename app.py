7acdbcf6f00a571ed0a1d14894953067ad6ba9c1
Harishhrds <140880280+Harishhrds@users.noreply.github.com>
2025-07-08 12:40:33 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import openai
+import requests
+
+
+st.title("Text To Image Generator (DALL·E)")
+api_key=st.sidebar.text_input("Enter OpenAI API Key")
+openai.api_key = api_key
+prompt = st.text_input("Enter your description")
+if prompt and api_key!="":
+    with st.spinner("Generating..."):
+     response = openai.images.generate(
+                model = "dall-e-3",
+                prompt=prompt,
+                quality = "standard",
+                size="1024x1024"
+     )
+     Image_url = response.data[0].url
+    
+    # download the image content
+     img_data = requests.get(Image_url).content
+      # Layout: image + download
+     col1, col2 = st.columns([4, 1])
+     with col1:
+      st.image(Image_url, caption="Generated Image", use_container_width=True)
+     with col2:
+        # add download button
+        st.download_button(
+            label="⬇️download image",
+            data = img_data,
+            file_name="image_from_prompt.png",
+            mime="image/png"
+        )
